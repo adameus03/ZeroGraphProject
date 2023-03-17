80b475f44292599af1a472e1fb85d2996b73382c
Amadeusz Sitnicki <amadeusz.sitnicki1@gmail.com>
2023-03-17 22:25:24 +0100

    Bisection and Regula Falsi pseudo buttons view added

---
+++ TopControlsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroGraphProject
+{
+    public class TopControlsViewModel
+    {
+        public double RootFindingOptionBoxWidth;
+    }
+}
